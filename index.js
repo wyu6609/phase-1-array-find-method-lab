e95cc50c742e1ba56ac695f4f877980b2c0c4435
William Yu <88666309+wyu6609@users.noreply.github.com>
2022-02-01 15:08:19 -0500

    commit tree 5dadea8cd96e3dfc21e10156d2e206ab32602a85

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+
+function superbowlWin(arrayOfObjects) {
+  let win = arrayOfObjects.find(FindWinningYear);
+  if (win !== undefined) {
+    return win["year"];
+  } else {
+    return undefined;
+  }
+}
+function FindWinningYear(item) {
+  return item.result === "W";
+}
